d3144d76c2098e48faced9240d36ea42cb9539ae
Grzegorz Śliwiński <fizyk@fizyk.net.pl>
2014-12-14 19:50:56 +0100

    added missing License headings

--- pyramid_decoy/views.py
+++ pyramid_decoy/views.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2014 by pyramid_decoy authors and contributors
+# <see AUTHORS file>
+#
+# This module is part of pyramid_decoy and is released under
+# the MIT License (MIT): http://opensource.org/licenses/MIT
+
 from pyramid.httpexceptions import HTTPFound
 
 
